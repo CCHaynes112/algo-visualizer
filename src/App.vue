e45c101b2cd6096c837c85305ed57f48d048b655
Curtis Haynes <CCHaynes1122@gmail.com>
2021-06-06 22:47:25 -0700

    Make bar and container different components

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,37 @@
 <template>
-  <div id="app">
+  <div>
     <div class="control-container">
+      Column Count
       <input type="text" v-model="columnCount" />
+      <br />
+      Sort Speed
+      <input type="number" v-model="sortSpeed" />
+      <br />
       <button v-on:click="generateColumns()">Generate Columns</button>
       <button v-on:click="bubbleSort()">Bubble Sort</button>
+      <button v-on:click="selectionSort()">Selection Sort</button>
+      <button v-on:click="insertionSort()">Insertion Sort</button>
     </div>
-    <div class="column-container">
-      <div
-        v-for="(column, index) in columns"
-        :key="index"
-        class="column"
-        :style="'height: ' + column + 'px; background-color: rgba(' + column + ', 0, ' + column + ', 1)'"
-      ></div>
-    </div>
+    <BarContainer />
   </div>
 </template>
 
 <script>
+import BarContainer from "./components/BarContainer.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: { BarContainer },
   data() {
-    return {
-      columnCount: 0,
-      columns: [],
-    };
+    return {};
   },
-  methods: {
-    getRandomInt(max) {
-      return Math.floor(Math.random() * max);
-    },
-    generateColumns() {
-      this.columns = [];
-      for (let i = 0; i < this.columnCount; i++) {
-        this.columns.push(this.getRandomInt(100));
-      }
-    },
-    bubbleSort() {
-      console.log("Sorting...");
-      let swapped;
-      do {
-        swapped = false;
-        for (let i = 0; i < this.columns.length; i++) {
-          if (this.columns[i] > this.columns[i + 1]) {
-            let tmp = this.columns[i];
-            this.columns[i] = this.columns[i + 1];
-            this.columns[i + 1] = tmp;
-            swapped = true;
-            this.$forceUpdate();
-          }
-        }
-      } while (swapped);
-      console.log("Done!");
-    },
-  },
+  created() {},
+  methods: {},
 };
 </script>
 
 <style>
-#app {
-}
 .control-container {
   padding: 15px 0 15px 0;
-}
-.column-container {
-  display: flex;
-}
-.column {
-  width: 10px;
-  background-color: blue;
 }
 </style>
---
+++ src/components/Bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="column" :style="style"></div>
+</template>
+
+<script>
+export default {
+  name: "Bar",
+  props: {
+    height: Number,
+    colorValue: Number,
+  },
+  computed: {
+    style() {
+      return (
+        "height: " + this.height + "px; background-color: rgb(" + this.colorValue + ", " + this.colorValue + ", 100);"
+      );
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+
+<style scoped>
+.column {
+    width: 20px;
+}
+</style>
